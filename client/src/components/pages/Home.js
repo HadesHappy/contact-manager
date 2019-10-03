b138ee866e7402c71091b5ca892f55a620818c2c
HadesHappy <happyhades123@gmail.com>
2019-10-03 14:53:15 +0200

    Implemented full CRUD and filter contacts functionality on UI

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
         <ContactForm />
       </div>
       <div>
+        <ContactFilter />
         <Contacts />
       </div>
     </div>
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -8,6 +8,7 @@ import {
   CLEAR_FILTER
 } from '../types';
 
+
 export default (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
@@ -15,12 +16,42 @@ export default (state, action) => {
         ...state,
         contacts: [...state.contacts, action.payload]
       };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id ? action.payload : contact
+        )
+      };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
           contact => contact.id !== action.payload
         )
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
+      };
+    case FILTER_CONTACTS:
+      return {
+        ...state,
+        filtered: state.contacts.filter(contact => {
+          const regex = new RegExp(`${action.payload}`, 'gi');
+          return contact.name.match(regex) || contact.email.match(regex);
+        })
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null
       };
     default:
       return state;
