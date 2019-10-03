79a2b9c24af652c0318f7390cb80051629fee667
HadesHappy <happyhades123@gmail.com>
2019-10-03 13:20:38 +0200

    Created form for adding, displaying, and deleting  contacts on UI

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='grid-2'>
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <Contacts />
+      </div>
     </div>
   );
 };
+
 
 export default Home;
