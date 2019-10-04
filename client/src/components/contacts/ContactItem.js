41716df996f7c7ddbda7320abc926c7752f445bc
HadesHappy <happyhades123@gmail.com>
2019-10-04 14:23:34 +0200

    Implemented delete contact functionality

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -7,10 +7,10 @@ const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
   const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
-  const { id, name, email, phone, type } = contact;
+  const { _id, name, email, phone, type } = contact;
 
   const onDelete = () => {
-    deleteContact(id);
+    deleteContact(_id);
     clearCurrent();
   };
 
