d3141fcc38013e1a7c51156afe15970e8d1efc42
HadesHappy <happyhades123@gmail.com>
2019-10-04 11:44:05 +0200

    Implemented user login functionality

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -20,6 +20,7 @@ export default (state, action) => {
         user: action.payload
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -29,6 +30,7 @@ export default (state, action) => {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
