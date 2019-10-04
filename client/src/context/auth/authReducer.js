a2930cca88e089bf3f4987c31ef17ed8ab4a9766
HadesHappy <happyhades123@gmail.com>
2019-10-04 11:31:21 +0200

    Loaded user and set token

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,6 +12,13 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
@@ -21,6 +28,7 @@ export default (state, action) => {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
