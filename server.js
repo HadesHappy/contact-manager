e4fd8a587cc1e21171b01f3779a7680806b81f7f
HadesHappy <happyhades123@gmail.com>
2019-10-07 19:02:27 +0200

    Some styling changes

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Manager App' })
-);
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
